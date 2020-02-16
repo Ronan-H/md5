1ece49cdae7333e46639c8035c67bd267da4c1a1
ronan <g00342393@gmit.ie>
2020-02-16 17:06:01 +0000

    Experimenting with F() macro and word typedef

--- md5.c
+++ md5.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <inttypes.h>
 
 // RFC: a "word" is a 32 bit quantity
 typedef uint32_t word;
@@ -10,7 +11,18 @@ int main() {
     // entry point of the program
     printf("Hello World!\n");
 
-    
+    word test = 0;
+    printf("Test value: %" PRIu32 "\n", test);
+    test++;
+    printf("Test value: %" PRIu32 "\n", test);
+
+    word x = 5;
+    word y = 10;
+    word z = 123;
+
+    word fx = F(F(x, y, z), y, z);
+
+    printf("fx: %" PRIu32 "\n", fx);
 
     return 0;
 }
