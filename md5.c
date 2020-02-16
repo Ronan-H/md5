3e0451069eb6b56faa587020eb99bd5c4d7096cd
ronan <g00342393@gmit.ie>
2020-02-16 17:00:53 +0000

    Added F() macro and word typedef

--- md5.c
+++ md5.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 
+// RFC: a "word" is a 32 bit quantity
+typedef uint32_t word;
+
+// function-like macro
+#define F(x, y, z) (x & y | (~x & z))
+
 int main() {
     // entry point of the program
     printf("Hello World!\n");
+
+    
+
     return 0;
 }
