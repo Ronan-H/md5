be9dcaec66c64a6dd8700e0fc5ab0eed527a4ef2
ronan <g00342393@gmit.ie>
2020-02-19 19:09:05 +0000

    Printing M

--- md5.c
+++ md5.c
@@ -29,48 +29,32 @@ struct Block {
 };
 
 // function declarations
-void printWordBits(word w);
 word * generateT();
 struct Block * readFileAsBlocks(char *filePath);
+void printWordBits(word w);
 
 int main() {
     // entry point of the program
-
-    word test = 0;
-    printf("Test value: %" PRIu32 "\n", test);
-    test++;
-    printf("Test value: %" PRIu32 "\n", test);
-
-    word x = 5231;
-    word y = 104321;
-    word z = 125433;
-
-    word fx = F(F(x, y, z), y, z);
-
-    printf("fx: %" PRIu32 "\n", fx);
-
-    printf("%-17s", "fx bits: ");
-    printWordBits(fx);
-
-    printf("%-17s", "ROTL fx by 17: ");
-    fx = ROTL(fx, 17);
-    printWordBits(fx);
-
     word *T = generateT();
 
     struct Block *M = readFileAsBlocks("md5.c");
+    struct Block *currBlock = M;
+    word *words = currBlock->words;
+    int blockCounter = 0;
+
+    while (currBlock != NULL) {
+        printf("BLOCK: %d\n", blockCounter++);
+
+        for (int i = 0; i < 16; i++) {
+            printWordBits(words[i]);
+        }
+
+        currBlock = currBlock->next;
+        words = currBlock->words;
+    }
+    printf("\n");
 
     return 0;
-}
-
-void printWordBits(word w) {
-    for (int i = 0; i < 32; i++) {
-        word bit = (w & 0x80000000) != 0;
-        printf("%" PRIu32, bit);
-        w = w << 1;
-    }
-
-    printf("\n");
 }
 
 // generate the 64 element array T, as per the RFC
@@ -80,7 +64,6 @@ word * generateT() {
 
     for (int i = 1; i <= 64; i++) {
         T[i] = (word)(T_MULTIPLIER * fabs(sin(i)));
-        printf("T[i]: %" PRIu32 "\n", T[i]);
     }
 
     return T;
@@ -147,4 +130,14 @@ struct Block * readFileAsBlocks(char *filePath) {
 
     // return head of linkedlist of Blocks
     return head;
+}
+
+void printWordBits(word w) {
+    for (int i = 0; i < 32; i++) {
+        word bit = (w & 0x80000000) != 0;
+        printf("%" PRIu32, bit);
+        w = w << 1;
+    }
+
+    printf("\n");
 }
